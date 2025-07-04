be6ac267bc1c241191b11b2b435eb777ecdcd41a
vikashxfriday <vikashxfriday@gmail.com>
2025-07-04 15:48:30 +0530

    Contact UI

--- application/views/themes/perfex/views/forgot_password.php
+++ application/views/themes/perfex/views/forgot_password.php
@@ -1,11 +1,17 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<style>
+a, label, .control-label, a:focus, a:hover {
+    color: #FFFFFF;
+}
+</style>
 <div class="mtop40">
     <div class="company-logo text-center">
             <?php echo get_company_logo(get_admin_uri() . '/', 'navbar-brand logo')?>       
 			</div>
     <div class="col-md-4 col-md-offset-4">
-        <div class="panel_s">
-            <div class="panel-body">
+        <div class="panel_s box-shadow-bg">
+            <div class="panel-body out-form">
+			
 			<h1 class="tw-font-semibold text-center mt-0 pt-0"><?php echo _l('customer_forgot_password_heading'); ?></h1>
                 <?php echo form_open($this->uri->uri_string(), ['id' => 'forgot-password-form']); ?>
                 <?php echo validation_errors('<div class="alert alert-danger text-center">', '</div>'); ?>
