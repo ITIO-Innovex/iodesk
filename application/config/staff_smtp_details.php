76123eef222bf777ddf6d5ecdf007edb087784c1
vikashitio <vikashg@itio.in>
2026-01-16 13:40:09 +0530

    SMTP

--- application/config/staff_smtp_details.php
+++ application/config/staff_smtp_details.php
@@ -6,6 +6,8 @@ defined('BASEPATH') or exit('No direct script access allowed');
 		$CI->db->order_by('priority', 'DESC');
 		$CI->db->limit(1);
 		$settings = $CI->db->select('*')->from(db_prefix() . 'webmail_setup')->get()->result_array();
+		
+		if (!empty($settings)) {
 		$settings=$settings[0];
 		//print_r($settings);exit;
 if(isset($settings['encryption'])&&$settings['encryption']&&isset($settings['mailer_smtp_host'])&&$settings['mailer_smtp_host']&&isset($settings['mailer_smtp_port'])&&$settings['mailer_smtp_port']&&isset($settings['mailer_email'])&&$settings['mailer_email']&&isset($settings['mailer_username'])&&$settings['mailer_username']&&isset($settings['mailer_password'])&&$settings['mailer_password']){
@@ -24,6 +26,10 @@ if(isset($settings['encryption'])&&$settings['encryption']&&isset($settings['mai
 			$config['smtp_crypto']  = trim($settings['encryption']);
 			$_SESSION['staff_fromemai']=$settings['mailer_email'];
 			$smtp_fetch_type="StaffSMTP";
+		}else{
+		$smtp_fetch_type_status=1;
+		}
+		
 		}else{
 		$smtp_fetch_type_status=1;
 		}
