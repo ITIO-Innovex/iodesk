2ac3f8aac2819140784d3bfcec6e9702774bd9dc
vikashitio <vikashg@itio.in>
2026-02-09 18:44:54 +0530

    Update

--- application/controllers/Cronjob.php
+++ application/controllers/Cronjob.php
@@ -263,6 +263,7 @@ $result = $this->services_subscriptions_model->send_renewal_email($email, $mailS
    // Download All Users Email
     public function download_email_from_cron_all()
     {
+	exit;
         
             // Increase execution time limit for email download
             set_time_limit(600); // 5 minutes
